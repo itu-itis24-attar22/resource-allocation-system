d865a2cd77b1d2fa7f9f8bc6672d2e7cf35b85be
Najib Attar <attar22@itu.edu.tr>
2026-04-28 19:35:29 +0300

    Iteration 20: refactor: use strategy factory in allocation service

--- src/services/AllocationService.h
+++ src/services/AllocationService.h
@@ -1,22 +1,24 @@
 #ifndef ALLOCATIONSERVICE_H
 #define ALLOCATIONSERVICE_H
 
+#include <string>
 #include <vector>
 #include "../models/Allocation.h"
+#include "../models/Request.h"
 #include "../models/OneTimeRequest.h"
 #include "../models/RecurringRequest.h"
 #include "../rules/RuleEngineFacade.h"
-#include "../strategies/GreedyAllocationStrategy.h"
+#include "../strategies/IAllocationStrategy.h"
 
 class AllocationService {
 private:
     std::vector<Allocation> allocations;
     RuleEngineFacade ruleEngineFacade;
-    GreedyAllocationStrategy defaultStrategy;
     const IAllocationStrategy* allocationStrategy;
 
 public:
     AllocationService();
+    explicit AllocationService(const std::string& strategyName);
     const std::vector<Allocation>& getAllocations() const;
     void addExistingAllocation(const Allocation& allocation);
     void processRequests(const std::vector<Request*>& requests);
